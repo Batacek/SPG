932efdd1a4902e37fbdfd25ca75e3154a07b44dc
Thomas <74470527+Batacek@users.noreply.github.com>
2024-06-05 17:43:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# -------------------------------------------------------------------------------------------------------------------------
+# |Imports|
+import random
+import string
+
+print(" ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ ")
+print("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌")
+print("▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ")
+print("▐░▌          ▐░▌       ▐░▌▐░▌          ")
+print("▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▄▄▄▄▄▄▄▄ ")
+print("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌▐░░░░░░░░▌")
+print(" ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌ ▀▀▀▀▀▀█░▌")
+print("          ▐░▌▐░▌          ▐░▌       ▐░▌")
+print(" ▄▄▄▄▄▄▄▄▄█░▌▐░▌          ▐░█▄▄▄▄▄▄▄█░▌")
+print("▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌")
+print(" ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀ ")
+
+
+while True:
+    # |Strings|
+    strength = input("Choose the strength:")
+    num = int(strength) + 1
+    string.gen = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890=´ú)¨ů§,.-|_:?"!%*><$ß¤×÷%ˇ;°'
+
+    # |Generation|
+    for a in range(1, num):
+        for b in range(a, num):
+            print(random.choice(string.gen), end='')
+
+    print("\n")
